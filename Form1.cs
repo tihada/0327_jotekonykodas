46d7d5c3621579ad8faca7c4941c5a27defb9deb
VorosDavidVasvari <12345678+VorosDavidVasvari@users.noreply.github.com>
2026-03-27 11:11:01 +0100

    added name adding, rm and getnames from file

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,41 @@ namespace _0327_jótékonykodás {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        void AddName(string filepath, string name) {
+            var fw = new StreamWriter(filepath, true);
+            fw.Write($"\n{name}");
+            fw.Close();
+        }
+
+        void RmName(string filepath, string name) {
+            var names = GetNames(filepath);
+            var fw = new StreamWriter(filepath);
+            if (!names.Remove(name)) {
+                MessageBox.Show("Nincs ilyen név a listában!", "Figyelem!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else {
+                for (Int32 i = 0; i < names.Count; i++) {
+                    fw.Write($"{names[i]}");
+                    if (i != names.Count - 1) {
+                        fw.Write("\n");
+                    }
+                }
+            }
+            fw.Close();
+        }
+
+        List<string> GetNames(string filepath) {
+            var fr = new StreamReader(filepath);
+            var names = new List<string>();
+
+            while (!fr.EndOfStream) {
+                names.Add(fr.ReadLine());
+            }
+
+            fr.Close();
+            return names;
         }
     }
 }
